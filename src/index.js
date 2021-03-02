83af1880f618f598eb85b1deb82c706f3f4b33cb
IrinaDash <irina.lev@list.ru>
2021-03-02 20:21:08 +0300

    reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+   let str = n + ""
+            let arrRev = str.split('')
+            arrRev.reverse()
+
+            let newStr = arrRev.join("")
+            newStr = parseInt(newStr)
+
+
+          return newStr
 }
